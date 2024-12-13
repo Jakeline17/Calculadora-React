a3b5226ddf6d299697958956d4eabd27f6d95e3a
jakeline17 <jaquelinesouza905@gmail.com>
2024-12-13 11:55:27 -0300

    atualizando

---
+++ src/components/Input/styles.js
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+
+export const InputContainer = styled.div`
+width:100%;
+height:100px;
+background-color:#1F1B1A;
+border: 1px solid #000;
+display:flex;
+align-items:center;
+justify-content:flex-end;
+border-radius:15px 15px 0 0;
+font-size:24px;
+font-family:'Roboto';
+
+
+input{
+width:80%;
+height:40px;
+background-color:#CED0CD;
+display:flex;
+flex-direction:column;
+align-items:flex-end;
+padding:0 10px;
+margin:10px 40px;
+font-size:2.5rem;
+font-family: "VT323", monospace;
+color:#555;
+box-shadow: inset 0px 8px 15px rgba(0, 0, 0, 0.4);
+border: 1px solid #403B3B;
+border-radius:5px;
+
+}
+`
---
+++ src/styles.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+width:100vw;
+height:100vh;
+background-color:#fff2f3;
+display:flex;
+align-items: center;
+justify-content: center;
+`
+
+export const Content = styled.div`
+background-color:#F8B6C6;
+width:40vh;
+height:410px;
+box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.4);
+border-radius:15px;
+
+`
+export const Row = styled.div`
+display:flex;
+flex-direction:row;
+justify-content:space-between;
+align-items:center;
+`
+
